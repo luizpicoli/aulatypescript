5f9638579b1aa2ef65781fa83402bb1f505b0f6f
luiz felipe picoli <felipedelibra@hotmail.com>
2021-09-13 19:35:11 -0300

    add excluir,colocar produto e add cliente

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import productRouter from './product.routes';
+import clientRouter from './client.routes';
 
 const routes = Router();
 
 routes.use('/products', productRouter);
+routes.use('/clients', clientRouter);
 
 export default routes;
